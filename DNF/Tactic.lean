0c5a254975c8a85bcd25eeb6c65f4a8b8480d62c
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2024-05-15 18:54:46 -0400

    trim other material

--- DNF/Tactic.lean
+++ DNF/Tactic.lean
@@ -11,13 +11,6 @@ def Lean.Expr.atoms (a : HashSet Expr) : Expr → HashSet Expr
   | Expr.app (Expr.const ``Not _) e => e.atoms a
   | e => a.insert e
 
-/-- Identify the type, LHS and RHS of an expression which may represent an equality. -/
-def Lean.Expr.eqAtoms (eqAtomsByType : HashMap Expr (HashSet Expr)) (e : Expr) :
-    HashMap Expr (HashSet Expr) :=
-  match e.eq? with
-  | some (ty, a, b) => eqAtomsByType.insert ty (((eqAtomsByType.findD ty {}).insert a).insert b)
-  | none => eqAtomsByType
-
 /-- Given an array of expressions which ought to contain all the atoms in a propositional logic
 expression, build up the SAT formula with atoms indexed by that array.  Fail if an atom is missing
 from the array. -/
@@ -38,43 +31,13 @@ def Lean.Expr.toSAT [Monad m] [MonadError m] (a : Array Expr) : Expr → m (SAT 
     | some i => pure (SAT.atom i)
     | none => throwError "error in parsing atoms of hypotheses"
 
-/-- Given an array of pairs `(ty, a) : Expr × Array Expr`, which ought to be such that each entry of
-`a` represents something of type `ty`, ... FIXME. -/
-def Lean.Expr.toEdge [Monad m] [MonadError m] (eqAtomsByType : Array (Expr × Array Expr))
-    (e : Expr) :
-    m <| Option (Σ i : Fin eqAtomsByType.size,
-      Fin (eqAtomsByType.get i).2.size × Fin (eqAtomsByType.get i).2.size) :=
-  match e.eq? with
-  | some (ty, a, b) =>
-    match (eqAtomsByType.map Prod.fst).indexOf? ty with
-    | some i₀ =>
-      let i : Fin eqAtomsByType.size := Fin.cast (eqAtomsByType.size_map Prod.fst) i₀
-      let eqAtoms : Array Expr := (eqAtomsByType.get i).snd
-      match eqAtoms.indexOf? a, eqAtoms.indexOf? b with
-      | some ia, some ib => pure <| some ⟨i, (ia, ib)⟩
-      | _, _ => throwError "error in parsing atoms of equality hypotheses"
-    | none => throwError "error in parsing atoms of equality hypotheses"
-  | none => pure none
-
 /-- Given proofs of the items in a list, prove `List.Forall` of the list. -/
 def proveForall : List Expr → MetaM Expr
   | [] => pure <| mkConst `trivial
   | [pf] => pure pf
   | hd :: tl => do mkAppM `And.intro #[hd, ← proveForall tl]
 
-/-- Given an expression which is the proof of `n` propositions chained together, build a length-`n`
-list comprising proofs of the indivitual expressions. -/
-def proofsOfProofForall (e : Expr) : Nat → MetaM (List Expr)
-  | 0 => pure []
-  | 1 => pure [e]
-  | n + 2 => do
-    let hd ← mkAppM ``And.left #[e]
-    let tl ← proofsOfProofForall (← mkAppM ``And.right #[e]) (n + 1)
-    pure (hd :: tl)
-
--- def extract (e : Expr) :
 def emptyArrayProp : Array Prop := #[]
-def emptyListSAT (n : Nat) : List (SAT (Fin n)) := []
 
 /-- Filter the nontrivial Prop hypotheses, build an expr-/
 def Lean.MVarId.atomsAndSATStructure (g : MVarId) : MetaM (Expr × Expr × Array Expr × Expr) := do
@@ -92,7 +55,6 @@ def Lean.MVarId.atomsAndSATStructure (g : MVarId) : MetaM (Expr × Expr × Array
   trace `Meta.tauto2 (fun _ => m!"found atoms {atoms}")
   let atomsE : Expr ← atoms.foldlM (fun a b => mkAppM `Array.push #[a, b]) (mkConst `emptyArrayProp)
   let atomsE : Expr ← mkAppM `Array.get #[atomsE]
-  -- trace `Meta.tauto2 (fun _ => m!"made atoms list {atomsE}")
 
   -- write the hypotheses as SAT formulas in these atoms
   let satsE : Expr := toExpr (← hs_types.mapM (Lean.Expr.toSAT atoms)).toList
@@ -100,29 +62,6 @@ def Lean.MVarId.atomsAndSATStructure (g : MVarId) : MetaM (Expr × Expr × Array
 
   let hE : Expr ← proveForall hs.toList
   pure (satsE, hE, atoms, atomsE)
-
-def foo (p : Expr × Expr × Array Expr × Expr) : MetaM Expr := do
-
-  let (satsE, hE, atoms, atomsE) := p
-
-  -- identify the atoms
-  let eqAtomsByType : HashMap Expr (HashSet Expr) := (atoms.foldl Lean.Expr.eqAtoms {})
-  let eqAtomsByTypeA : Array (Expr × Array Expr) :=
-    eqAtomsByType.toArray.map (fun (a, b) => (a, HashSet.toArray b))
-  trace `Meta.tauto2 (fun _ => m!"found equality atoms {eqAtomsByTypeA}")
-
-  let n : Nat := eqAtomsByTypeA.size
-  let tys (i : Fin n) : Expr := (eqAtomsByTypeA.get i).1
-  let m (i : Fin n) : Nat := (eqAtomsByTypeA.get i).2.size
-  let eqAtoms (i : Fin n) (j : Fin (m i)) : Expr := (eqAtomsByTypeA.get i).2.get j
-
-  let b : Array (Option (Σ i : Fin n, Fin (m i) × Fin (m i))) ← atoms.mapM (Lean.Expr.toEdge eqAtomsByTypeA)
-
-
-
-  let a ← mkAppM `List.dnf #[satsE]
-  mkAppM `List.interpretDNF_of_interpret #[satsE, atomsE, hE]
-
 
 /-- Tactic which proves `False`, if possible, from the pure propositional logic structure of the
 hypotheses.  Implementation is a proof by reflection from a naive computation of the hypotheses'
