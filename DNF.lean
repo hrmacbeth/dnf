69362739d7fffebca4abeabf920023441bbe5852
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2024-09-26 14:13:41 -0400

    import all

--- DNF.lean
+++ DNF.lean
@@ -1,5 +1,4 @@
--- This module serves as the root of the `Dnf` library.
--- Import modules here that should be built as part of the library.
+import DNF.Test.Theory
 import DNF.Theory.List
 import DNF.Theory.ProveFalseFromSAT
 import DNF.Tactic
