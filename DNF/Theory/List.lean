fc5925e9906694047b0c69e53f8b693475f1314b
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2024-09-26 10:32:40 -0400

    timing tests

--- DNF/Theory/List.lean
+++ DNF/Theory/List.lean
@@ -1,19 +1,11 @@
 /- Copyright (c) Heather Macbeth, 2023.  All rights reserved. -/
 import Batteries.Data.List.Basic
+import Mathlib.Data.List.Defs
 import Mathlib.Data.SProd
 
 -- from `Mathlib.Data.List.Defs`
 instance instSProd : SProd (List α) (List β) (List (α × β)) where
   sprod := List.product
-
-/-- `l.Forall p` is equivalent to `∀ a ∈ l, p a`, but unfolds directly to a conjunction, i.e.
-`List.Forall p [0, 1, 2] = p 0 ∧ p 1 ∧ p 2`. -/
--- from `Mathlib.Data.List.Defs`
-@[simp]
-def List.Forall (p : α → Prop) : List α → Prop
-  | [] => True
-  | x :: [] => p x
-  | x :: l => p x ∧ Forall p l
 
 def List.Exists (p : α → Prop) : List α → Prop
   | [] => False
