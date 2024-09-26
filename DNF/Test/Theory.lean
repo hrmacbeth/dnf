55b54cad87aeadf27885bd2c1c061f5744cc2639
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2024-09-26 13:55:28 -0400

    cleaner tests

--- DNF/Test/Theory.lean
+++ DNF/Test/Theory.lean
@@ -2,38 +2,24 @@ import Mathlib.Data.Matrix.Notation
 import Mathlib.Util.Time
 import DNF.Theory.ProveFalseFromSAT
 
-set_option maxHeartbeats 400 in
-example : True := by
-  let l := [(SAT.atom (0:Fin 2)).or (SAT.atom 1), (SAT.atom 0).not, (SAT.atom 1).not]
-  have : determineSAT l = false := by
-    delta determineSAT List.dnf
-    dsimp only
-    eq_refl
-  trivial
-
-set_option maxHeartbeats 300 in
--- #time
-example (P Q : Prop) (h1 : P ∨ Q) (h2 : ¬ P) (h3 : ¬ Q) : False := by
+set_option maxHeartbeats 500 in
+example (P Q : Prop) (h1 : P ∨ Q) (h2 : ¬ P) (h3 : ¬ Q) : False :=
   let l : List (SAT (Fin 2)) :=
-    [(SAT.atom 0).or (SAT.atom 1), (SAT.atom 0).not, (SAT.atom 1).not]
+    [(SAT.atom 0).or (SAT.atom 1), (SAT.atom 0).not, (SAT.atom 1).not]; by
   refine proveFalseFromSAT l ?_ ![P, Q] ⟨h1, h2, h3⟩
-  delta determineSAT List.dnf
-  dsimp only
   eq_refl
 
-set_option maxHeartbeats 2000 in
--- #time
 example (p q r : Prop)
     (h : (( (p ∨ (q ∧ r)) ∧ ¬((p ∨ q) ∧ (r ∨ p ∨ r)))
        ∨  (¬(p ∨ (q ∧ r)) ∧  ((p ∨ q) ∧ (r ∨ p ∨ r))))) :
-    False := by
+    False :=
   let a : SAT (Fin 3) := (SAT.atom 0).or ((SAT.atom 1).and (SAT.atom 2))
   let b1 : SAT (Fin 3) := (SAT.atom 0).or (SAT.atom 1)
   let b2 : SAT (Fin 3) := (SAT.atom 2).or ((SAT.atom 0).or (SAT.atom 2))
   let b : SAT (Fin 3) := b1.and b2
   let e : SAT (Fin 3) := (a.and b.not).or (a.not.and b)
-  let l : List (SAT (Fin 3)) := [e]
+  let l : List (SAT (Fin 3)) := [e]; by
   refine proveFalseFromSAT l ?_ ![p, q, r] h
   delta determineSAT List.dnf SAT.dnf
-  dsimp only
+  dsimp only -- times out without this
   eq_refl
