d22cfd3937afea623a5fa4d6d15431585c4a6447
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2024-09-26 14:15:40 -0400

    bump to 4.8.0

--- lakefile.lean
+++ lakefile.lean
@@ -1,34 +1,15 @@
 import Lake
 open Lake DSL
 
-def moreServerArgs := #[
-  "-Dpp.unicode.fun=true", -- pretty-prints `fun a ↦ b`
-  "-Dpp.proofs.withType=false",
-  "-DwarningAsError=false"
-]
-
--- These settings only apply during `lake build`, but not in VSCode editor.
-def moreLeanArgs := moreServerArgs
-
--- These are additional settings which do not affect the lake hash,
--- so they can be enabled in CI and disabled locally or vice versa.
--- Warning: Do not put any options here that actually change the olean files,
--- or inconsistent behavior may result
-def weakLeanArgs : Array String :=
-  if get_config? CI |>.isSome then
-    #["-DwarningAsError=true"]
-  else
-    #[]
-
-package «dnf» where
-  moreServerArgs := moreServerArgs
-  -- add any package configuration options here
+package dnf where
+  leanOptions := #[
+    ⟨`pp.unicode.fun, true⟩, -- pretty-prints `fun a ↦ b`
+    ⟨`pp.proofs.withType, false⟩,
+    ⟨`warningAsError, false⟩
+  ]
 
 require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git"
+  "https://github.com/leanprover-community/mathlib4.git" @ s!"v{Lean.versionString}"
 
 @[default_target]
-lean_lib «DNF» where
-  moreLeanArgs := moreLeanArgs
-  weakLeanArgs := weakLeanArgs
-  -- add any library configuration options here
+lean_lib DNF
